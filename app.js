e29e05181c5e98b01ab5e7dba3d11e1b44e260f0
ehye <yjw9610@hotmail.com>
2024-03-19 19:03:48 +0800

    11.6

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const express = require("express");
-const app = express();
+import express, { static as static_express } from 'express'
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.use(static_express('dist'))
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+app.listen(PORT, () => {})
