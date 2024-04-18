81243677cb5d2d265f7d8101f96a45ebc86f612e
ehye <yjw9610@hotmail.com>
2024-04-18 15:30:04 +0800

    11.10

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-import express, { static as static_express } from 'express'
+const express = require('express')
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(static_express('dist'))
+app.use(express.static('dist'))
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {})
