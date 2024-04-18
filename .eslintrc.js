e2071b77480fcc413e7f5a19ebe9534bc76a3941
ehye <yjw9610@hotmail.com>
2024-04-18 16:17:27 +0800

    windows line break

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,10 +24,10 @@ module.exports = {
       "error",
         2
       ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
+    // "linebreak-style": [
+    //   "error",
+    //   "windows"
+    // ],
     "quotes": [
       "error",
       "single"
